a17f1b5142c3c54ba7c14adb2e9dd786c7920cc0
Manojs <manojs.nitt@gmail.com>
2011-12-17 01:01:10 +0530

    SmartCollege project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartCollege::Application.config.secret_token = 'd2738971a4294fa14d9fdcd2a171c63d6dedb735e9154fa2ccb27143b236b34d458ff792e17b856de2c18bc0661ed6e816699198c27df744f8175f1db91e8406'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SmartCollege::Application.config.session_store :cookie_store, key: '_SmartCollege_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SmartCollege::Application.config.session_store :active_record_store
