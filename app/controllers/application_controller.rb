b489505ac80ab690119c314edca08c3f91793e81
Manojs <manojs.nitt@gmail.com>
2011-12-23 23:04:42 +0530

    Devise has implemented for user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate_user!
+
+  # ======== Devise related method =========
+  # default redirect to is "/users/sign_in"
+  def after_sign_out_path_for(resource_or_scope)
+    login_url
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111221173135) do
+ActiveRecord::Schema.define(:version => 20111222190020) do
 
   create_table "course_subjects", :force => true do |t|
     t.integer  "course_id"
@@ -95,6 +95,15 @@ ActiveRecord::Schema.define(:version => 20111221173135) do
   add_index "institutes", ["address_id"], :name => "index_institutes_on_address_id"
   add_index "institutes", ["contact_person_id"], :name => "index_institutes_on_contact_person_id"
 
+  create_table "subject_categories", :force => true do |t|
+    t.integer  "name_id"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "subject_categories", ["name_id"], :name => "index_subject_categories_on_name_id"
+
   create_table "subjects", :force => true do |t|
     t.string   "name"
     t.string   "code"
@@ -128,6 +137,23 @@ ActiveRecord::Schema.define(:version => 20111221173135) do
     t.string   "known_diseases"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
+
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
